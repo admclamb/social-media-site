ab01c74601769488fb1b23fbebb73a864d362517
admclamb <adylanmclamb@yahoo.com>
2022-10-23 14:52:05 -0400

    adding create post client

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Card.css';
 type Props = {
   children: React.ReactNode;
   background: string;
--- client/src/components/Create/CreatePostFeed/CreatePostFeed.tsx
+++ client/src/components/Create/CreatePostFeed/CreatePostFeed.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import Card from '../../Card/Card';
 import ProfileIcon from '../../Icons/ProfileIcon/ProfileIcon';
+import { Link } from 'react-router-dom';
 import './CreatePostFeed.css';
 type Props = {};
 
 const CreatePostFeed = (props: Props) => {
   return (
     <Card classes="create-post-feed">
-      <ProfileIcon />
-      <form className="create-post-feed--form">
-        <input type="text" className="input" placeholder="What's happening?" />
-      </form>
+      <div className="create-post-feed--header">
+        <ProfileIcon />
+        <form className="create-post-feed--form">
+          <input
+            type="text"
+            className="input"
+            placeholder="What's happening?"
+          />
+        </form>
+      </div>
+      <div className="create-post-feed--links">
+        <Link to="/create/photo">
+          <i className="fa-solid fa-image text-secondary fa-lg mr-sm"></i> Photo
+        </Link>
+        <Link to="/create/video">
+          <i className="fa-solid fa-video text-secondary fa-lg mr-sm"></i> Video
+        </Link>
+        <Link to="/create/article">
+          <i className="fa-solid fa-newspaper text-secondary fa-lg mr-sm"></i>
+          Article
+        </Link>
+      </div>
     </Card>
   );
 };
